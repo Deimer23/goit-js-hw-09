743bd1bfa7632bd194df84aa46a92ce5536e6db3
Deimer23 <116132047+Deimer23@users.noreply.github.com>
2023-04-30 02:58:40 +0000

    Deploying to gh-pages from @ Deimer23/goit-js-hw-09@a5a1a52493b8180ff47112c2f074495aefc2423b 🚀

---
+++ 01-color-switcher.9410ca5d.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),n=document.querySelector("body");let a;t.addEventListener("click",(function(e){e.preventDefault(),t.disabled=!0,a=setInterval((()=>{n.style.background=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3)})),e.addEventListener("click",(function(e){clearInterval(a),t.disabled=!1}));
+//# sourceMappingURL=01-color-switcher.9410ca5d.js.map
